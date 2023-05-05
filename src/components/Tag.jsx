8979f1d015d727ffaab6b26e8d1c242f8b29bd05
SALVADORPOETA <dsml@live.com.mx>
2023-05-05 01:08:53 -0600

    wh1

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -8,7 +8,7 @@ const Tag = () => {
     return (
         <div className='container'>
             <h1>Random {tag} Gif</h1>
-            <img width={250} height={250} src={gif} alt="Random Gif" />
+            <img width={250} src={gif} alt="Random Gif" />
             <h2>Search gif:</h2>
             <label aria-label='Write down a category of a gif' ><input value={tag} onChange={(e) => setTag(e.target.value)} type="text" /></label>
             <button onClick={() => fetchGif(tag)}>CLICK FOR NEW</button>
